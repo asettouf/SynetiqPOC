337f420cdc29021bc4bd9a16c7deb3ea97e8324b
Adonis <adonis.settouf@gmail.com>
2015-11-05 11:01:43 +0100

    Adding local storage capabilities

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,8 @@
 //@author Adonis Settouf <adonis.settouf@gmail.com>
 
+
+//@param INTERVAL_DELTA int - set the delta of execution of the setInterval used for recording
+var INTERVAL_DELTA = 1000;
 //@param cursorX int - position of the cursor along the x axis
 var cursorX = 0;
 //@param currentUserId int - User created for this recording
@@ -16,11 +19,14 @@ var timeWhenVideoEnds = 10;
 var currentTimeOfTheVideo = 0;
 //@param resultOfTheUser array - ecording in a hash the user rating
 var resultOfTheUser = [];
+//@param isConnected boolean - Switch to false if connection is interrupted
+var isConnected = true;
+//@param backupArray array - Array stored in local storage in case of connection failure
+var backupArray = {};
 
 
 $(document).ready(function(){
     main();
-    retrieveUserId();
 });
 
 //principal function to be run once document is ready
@@ -28,17 +34,43 @@ var main = function(){
     drawScale("scale", 10);
     drawCursor("cursor");
     moveObject("scale");
+	retrieveUserId();
     var intervalID = setInterval(function(){
         if(currentTimeOfTheVideo >= timeWhenVideoEnds){
-            //sendData();
             clearInterval(intervalID);
         }
-        recordPosition();
-        sendOneSecond();
-    }, 1000);
+		
+		recordPosition();
+		checkConnection();
+		if (isConnected){
+			sendOneSecond();
+		}else{
+			createBackupArray();
+		}
+		
+    }, INTERVAL_DELTA);
 };
 
+var checkConnection = function(){
+	$.ajax({
+	type: "GET",
+	url: "../php/sendDataToDB.php",
+	data: "isConnected"
+	})
+	.done(function(data){
+        isConnected = true;
+    }).error(function(error){
+        isConnected = false;
+    });
+}
 
+var createBackupArray = function(){
+	backupArray = JSON.parse(localStorage.getItem("backupArray"));
+	backupArray.currentUserValues.push({"Second" : currentSecond,
+	"Value" : currentCursorValue});
+	localStorage.setItem("backupArray", JSON.stringify(backupArray));
+}
+	
 //return an array with values between 0 and 10, and of length timeWhenVideoEnds
 //@return array - array with random values between 0 and 10
 var generateRandomArray = function(){
@@ -54,12 +86,15 @@ var generateRandomArray = function(){
 var retrieveUserId = function(){
     $.ajax({
 	type: "GET",
-	url: "/php/sendDataToDB.php",
+	url: "../php/sendDataToDB.php",
 	data: "uid"
 	})
 	.done(function(data){
         console.log(data);
-        currentUserId = data;
+        currentUserId = data.split("<")[0];
+		backupArray = {currentUserValues: []};
+		localStorage.setItem("backupArray", JSON.stringify(backupArray));
+		console.log(currentUserId);
     }).error(function(error){
         console.log(error);
     });
@@ -69,7 +104,7 @@ var sendData = function(){
     var randarray = generateRandomArray();
     $.ajax({
         type: "POST",
-        url: "/php/sendDataToDB.php",
+        url: "../php/sendDataToDB.php",
         data: "test="+ JSON.stringify(randarray),
         dataType: "html"
     })
@@ -83,20 +118,20 @@ var sendData = function(){
 
 //post one second to DB
 var sendOneSecond = function(){
-    console.log(currentSecond);
     $.ajax({
         type: "POST",
-        url: "/php/sendDataToDB.php",
+        url: "../php/sendDataToDB.php",
         data: {"uid" : currentUserId,
                 "second" : currentSecond,
                 "value" : currentCursorValue
         }
     })
-        .done(function(data){
-            console.log(data);
-        }).fail(function(error){
-            console.log(error);
-        });
+	.done(function(data){
+		console.log(data);
+	}).fail(function(error){
+		isConnected = false;
+		console.log(error);
+	});
 }
 
 //record the current position of the cursor every second, we round to the
